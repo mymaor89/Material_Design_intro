65d69bf5abac6b5c2714fb99348e213e8d85b8fb
Maor Yatskan <mymaor89@gmail.com>
2020-04-30 19:00:40 +0300

    Drawer implementation

--- app/src/main/java/com/example/tabsexample/DrawerActivity.java
+++ app/src/main/java/com/example/tabsexample/DrawerActivity.java
@@ -1,14 +1,51 @@
 package com.example.tabsexample;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class DrawerActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+        toolbar = findViewById(R.id.main_toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.openNav, R.string.closeNav
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_hardware:
+                        Toast.makeText(DrawerActivity.this, "hardware", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.nav_software:
+                        Toast.makeText(DrawerActivity.this, "software", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.nav_lecturer:
+                        Toast.makeText(DrawerActivity.this, "lecturer", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }

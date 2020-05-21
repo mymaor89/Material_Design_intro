5cf46b71b4d734b94ee0e3536ae63a7c3aa8ab77
Maor Yatskan <mymaor89@gmail.com>
2020-05-21 17:59:42 +0300

    Floating button added

--- app/src/main/java/com/example/tabsexample/DrawerActivity.java
+++ app/src/main/java/com/example/tabsexample/DrawerActivity.java
@@ -6,19 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class DrawerActivity extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-
+    FloatingActionButton fab;
+    FragmentManager fm;
+    FragmentTransaction ft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,18 @@ public class DrawerActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.main_toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
+        fab = findViewById(R.id.fab_add_issue);
+        fm = getSupportFragmentManager();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ft = fm.beginTransaction();
+                AddIssueFragment asf = new AddIssueFragment();
+                ft.replace(R.id.frag_container, asf);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
         setSupportActionBar(toolbar);
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.openNav, R.string.closeNav
@@ -35,13 +51,11 @@ public class DrawerActivity extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                FragmentManager fm;
-                FragmentTransaction ft;
+
                 drawerLayout.closeDrawers();
                 switch (item.getItemId()) {
                     case R.id.nav_hardware:
-                        //Toast.makeText(DrawerActivity.this, "hardware", Toast.LENGTH_LONG).show();
-                        fm = getSupportFragmentManager();
+                        //Toast.makeText(DrawerActivity.this, "hardware", Toast.LENGTH_LONG).show()
                         HardwareFragment hf = new HardwareFragment();
                         ft = fm.beginTransaction();
                         ft.replace(R.id.frag_container, hf);
